6d4b793906eb6dc6d19533284d69e4ce4048d6f3
Nandinis1024 <nandini.sunil1024@gmail.com>
2023-08-29 01:20:55 +0530

    mongo cloud cluster

--- app.js
+++ app.js
@@ -26,7 +26,7 @@ const User = require("./models/users");
 const mongoSanitize = require('express-mongo-sanitize');
 const helmet = require('helmet');
 const MongoDBStore = require("connect-mongo")(session);
-const dbUrl = 'mongodb://127.0.0.1:27017/yelp-camp';
+const dbUrl = process.env.DB_URL || 'mongodb://127.0.0.1:27017/yelp-camp';
 
 // mongodb://127.0.0.1:27017/yelp-camp
 mongoose.connect(dbUrl,{
