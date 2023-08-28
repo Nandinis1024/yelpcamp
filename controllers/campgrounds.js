371e997d03d1909f2c05442632ca14d5c49913a7
Nandini Sharma <114241516+Nandinis1024@users.noreply.github.com>
2023-08-29 01:57:51 +0530

    Update campgrounds.js

--- controllers/campgrounds.js
+++ controllers/campgrounds.js
@@ -1,6 +1,6 @@
 const Campground = require("../models/campgrounds");
 const { campgroundSchema } = require('../schemas.js');
-const ExpressError = require('../utils/expressError');
+const ExpressError = require('../utils/ExpressError');
 const { cloudinary } = require("../cloudinary")
 const mbxGeocoding = require("@mapbox/mapbox-sdk/services/geocoding");
 const mapBoxToken = process.env.MAP_BOX;
